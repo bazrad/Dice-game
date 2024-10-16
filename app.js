2669c412d93e7d94f6ce330621b1e196d12a8528
bazrad <bazrad13@gmail.com>
2024-10-16 15:33:05 +0800

    Shoo shiddeg bolloo

--- app.js
+++ app.js
@@ -4,17 +4,26 @@ var scores = [0, 0];
 
 var roundScore = 0;
 
-var dice = Math.floor(Math.random() * 6) + 1;
+
 
 // <div class="player-score" id="score-0">43</div>
 // window.document.querySelector("#score-0").textContent = dice;
 // document.querySelector("#score-1").innerHTML = "<em>Yes!<em>";
 
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
+document.getElementById("score-0").textContent = 0;// mash hurdan ajildah function
+document.getElementById("score-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
+var diceDOM = document.querySelector(".dice");
+diceDOM.style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function(){
+    var dice = Math.floor(Math.random() * 6) + 1;
+    diceDOM.style.display = "block";
+    diceDOM.src = 'dice-' + dice + '.png';
+    
+});
 
-document.querySelector(".dice").style.display = "none";
 
-console.log(":" + dice);
+function diceShid(){
+    alert("Shoo buulee " + dice );
+}
