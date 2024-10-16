079802f70c6b2097c949ee1ab5be5bba20549a43
bazrad <bazrad13@gmail.com>
2024-10-16 17:01:31 +0800

    Active hesgiig solih heseg

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var activePlayer = 1;
+var activePlayer = 0
 
 var scores = [0, 0];
 
@@ -6,24 +6,36 @@ var roundScore = 0;
 
 
 
-// <div class="player-score" id="score-0">43</div>
-// window.document.querySelector("#score-0").textContent = dice;
-// document.querySelector("#score-1").innerHTML = "<em>Yes!<em>";
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
-document.getElementById("score-0").textContent = 0;// mash hurdan ajildah function
-document.getElementById("score-1").textContent = 0;
-document.getElementById("current-0").textContent = 0;
-document.getElementById("current-1").textContent = 0;
-var diceDOM = document.querySelector(".dice");
-diceDOM.style.display = "none";
-document.querySelector(".btn-roll").addEventListener("click", function(){
-    var dice = Math.floor(Math.random() * 6) + 1;
-    diceDOM.style.display = "block";
-    diceDOM.src = 'dice-' + dice + '.png';
-    
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+
+document.querySelector(".btn-roll").addEventListener("click", function() {
+
+var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+
+diceDom.style.display = "block";
+
+
+diceDom.src = "dice-" + diceNumber + ".png";
+
+if (diceNumber !== 1) {
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+} else {
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = 0;
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    diceDom.style.display = "none";
+}
 });
 
 
-function diceShid(){
-    alert("Shoo buulee " + dice );
-}
+// @
